1c5594a57745070d69adf936b9c2bcb7f54fd677
lsw94 <37287282+lsw94@users.noreply.github.com>
2019-04-05 23:37:57 +0800

    ajax to be done to pass filter data

--- SearchEngine/views.py
+++ SearchEngine/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.http import HttpResponseRedirect
 
 # from Backend.search import search_string
 from .forms import SearchForm, FilterForm
+import SearchEngine.Backend.search as search
 # from Backend import
 # Create your views here.
 
@@ -12,31 +13,52 @@ def index(request):
     if request.method == "GET":
         form = SearchForm()
 
-    return render(request, "index.html", {'form': form})
+        return render(request, "index.html", {'form': form})
 
 
 def result_view(request):
-
     if request.method == 'GET':
         form = SearchForm(request.GET)
-        filters = FilterForm(request.GET)
+        filter_form = FilterForm(request.GET)
 
         if not form.is_valid():
             return redirect('/')
 
+        if not filter_form.is_valid():
+            filter_form = FilterForm()
+
+       # filters = request.GET.get('filters')
+        print(request.GET)
+
         query = str(request.GET.get('query'))
-        # results = search_string(query)
-
-
-
-    # query results to be inserted here
-
+        results = search.search_string(query)
         context = {
             "form": form,
-            "filter": filters,
-            "results": ["doc 1", "doc 2","doc 3", "doc 4"]
+            "filter": filter_form,
+            "results": results
         }
+
+    elif request.method == 'POST':
+        form = SearchForm(request.GET)
+        filter_form = FilterForm(request.POST)
+
+        if not filter_form.is_valid():
+            filter_form = FilterForm()
+
+        print(request.POST)
+        print("test");
+
+        query = str(request.GET.get('query'))
+        results = search.search_string(query)
+        context = {
+            "form": form,
+            "filter": filter_form,
+            "results": results
+        }
+
     else:
         return redirect('/')
+
+
 
     return render(request, "result.html", context)
