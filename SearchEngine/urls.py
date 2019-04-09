56df195e58a2cf9333bc377aff55a416773a91c0
Luke <37287282+lsw94@users.noreply.github.com>
2019-04-09 16:27:51 +0800

    update paging

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/', views.result_view, name='search')
+    path('search/', views.result_view, name='search'),
 ]
