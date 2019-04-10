d93d88b0cfe505deff067b793439a2a22a2bd4dc
Luke <37287282+lsw94@users.noreply.github.com>
2019-04-10 17:37:05 +0800

    fixed url query parameter duplication and persistent filter on page change done

--- CZ4034_Assignment/settings.py
+++ CZ4034_Assignment/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '_7n9p%f=2v(0h4d$utmhkipnmf%=g(&gs2red21jby9wbxml=#'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,8 +68,9 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'CZ4034_Assignment.wsgi.application'
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
+WSGI_APPLICATION = 'CZ4034_Assignment.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -82,7 +81,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -102,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -115,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- SearchEngine/views.py
+++ SearchEngine/views.py
@@ -1,12 +1,11 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
-from django.http import HttpResponseRedirect
+import json
 
-# from Backend.search import search_string
 from .forms import SearchForm, FilterForm
 import SearchEngine.Backend.search as search
-import time
+
+
 # from Backend import
 # Create your views here.
 
@@ -14,28 +13,44 @@ import time
 def index(request):
     if request.method == "GET":
         form = SearchForm()
-        print(request.GET)
+        if 'filtering' in request.session:
+            del request.session['filtering']
+        if 'form_data' in request.session:
+            del request.session['form_data']
+        request.session.modified = True
+
         return render(request, "index.html", {'form': form})
 
 
 def result_view(request):
     if request.method == 'GET':
         form = SearchForm(request.GET)
-        filter_form = FilterForm(request.GET)
+        filters = ""
+        if 'filtering' in request.session and 'form_data' in request.session:
+            if len(request.session['form_data']) > 1:
+                data = dict()
+                for d in request.session['form_data'][1:]:
+                    data.update({d.get('name'): d.get('value')})
+                filter_form = FilterForm(initial=data)
+            else:
+                filter_form = FilterForm()
+            filters = request.session['filtering']
+
+        else:
+            filter_form = FilterForm()
 
         if not form.is_valid():
             return redirect('/')
 
-        if not filter_form.is_valid():
-            filter_form = FilterForm()
-
-       # filters = request.GET.get('filters')
-
-       # print(request.GET)
-
         query = str(request.GET.get('query'))
-        results, auto_correct, duration = search.search_string(query)
-        paginator = Paginator(results, 20) # show 20 page
+        searchresults, auto_correct, duration = search.search_string(query)
+
+        if not filters:
+            results = searchresults
+        else:
+            results = [document for document in searchresults if document.category in filters]
+
+        paginator = Paginator(results, 20)  # show 20 page
         page = request.GET.get('page')
         page_results = paginator.get_page(page)
 
@@ -55,16 +70,19 @@ def result_view(request):
         if not filter_form.is_valid():
             filter_form = FilterForm()
 
+        else:
+            request.session['filtering'] = request.POST.getlist('filters[]')
+            jlist = json.loads(request.POST.get('form_data[]'))
+            request.session['form_data'] = jlist
+
         query = str(request.GET.get('query'))
         searchresults, auto_correct, duration = search.search_string(query)
         filters = request.POST.getlist('filters[]')
-        print(filters)
 
         if not filters:
             results = searchresults
         else:
             results = [document for document in searchresults if document.category in filters]
-            print(results)
 
         paginator = Paginator(results, 20)  # show 20 page
         page = request.GET.get('page')
